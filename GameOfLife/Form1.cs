7a3d69cea1468d6dc730658a6e2e3dc19c7b2004
Denys Leon <leonden0046@gmail.com>
2024-12-19 17:26:26 +0200

    Add project files.

---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+
+        private GameEngine gameEngine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame()
+        {
+            if (timer1.Enabled)
+                return;
+
+            nudDensity.Enabled = false;
+            nudResolution.Enabled = false;
+
+            resolution = (int)nudResolution.Value;
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+
+            int maxDensityValue = (int)nudDensity.Maximum;
+            int invertedDensityValue = (int)nudDensity.Minimum + maxDensityValue - (int)nudDensity.Value;
+            gameEngine = new GameEngine(height / resolution, width / resolution, invertedDensityValue);
+
+            pictureBox1.Image = new Bitmap(width, height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            Text = $"Generation {gameEngine.CurrentGeneration}";
+            timer1.Start(); // start game
+        }
+
+        private void StopGame()
+        {
+            if (timer1.Enabled == false)
+                return;
+
+            timer1.Stop();
+            nudDensity.Enabled = true;
+            nudResolution.Enabled = true;
+        }
+
+        private void DrawNextGeneration()
+        {
+            graphics.Clear(Color.Black);
+
+            bool[,] field = gameEngine.GetCurrentGeneration();
+
+
+            for (int x = 0; x < field.GetLength(0); x++)
+            {
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    if (field[x,y])
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
+                }
+            }
+
+
+            pictureBox1.Refresh();
+
+            Text = $"Generation: {gameEngine.CurrentGeneration}";
+
+            gameEngine.GenerateNextGeneration();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawNextGeneration();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (timer1.Enabled == false)
+                return;
+
+            int xCoord = e.Location.X / resolution;
+            int yCoord = e.Location.Y / resolution;
+
+            if (e.Button == MouseButtons.Left) // add life at coord
+            {
+                gameEngine.AddCell(xCoord, yCoord);
+            }
+
+            if (e.Button == MouseButtons.Right) // destroy life at coord
+            {
+                gameEngine.RemoveCell(xCoord, yCoord);
+            }
+        }
+    }
+}
---
+++ GameOfLife/GameEngine.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public class GameEngine
+    {
+        public uint CurrentGeneration { get; private set; }
+
+        private bool[,] field; // know cell position and whether it is alive
+        private readonly int rows;
+        private readonly int cols;
+
+        public GameEngine(int rows, int cols, int density)
+        {
+            this.rows = rows;
+            this.cols = cols;
+
+            Random random = new Random();
+
+            field = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next(density) == 0;
+                }
+            }
+        }
+
+        public void GenerateNextGeneration()
+        {
+            bool[,] newField = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    int neighborsCount = CountNeighbors(x, y);
+                    bool hasLife = field[x, y];
+
+                    if (!hasLife && neighborsCount == 3) // new life is born
+                    {
+                        newField[x, y] = true;
+                    }
+                    else if (hasLife && (neighborsCount < 2 || neighborsCount > 3)) // cell dies
+                    {
+                        newField[x, y] = false;
+                    }
+                    else
+                    {
+                        newField[x, y] = hasLife; // field[x,y] // leave as it is
+                    }
+                }
+            }
+
+            field = newField;
+            CurrentGeneration++;
+        }
+
+        public bool[,] GetCurrentGeneration()
+        {
+            // Because array is referency type, we dont want to change the source array somewhere outside this class
+            bool[,] fieldCopy = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    fieldCopy[x, y] = field[x, y];
+                }
+            }
+
+            return fieldCopy;
+        }
+
+        public void AddCell(int x, int y)
+        {
+            field[x, y] = true;
+        }
+
+        public void RemoveCell(int x, int y)
+        {
+            field[x, y] = false;
+        }
+
+        private void UpdateCellState(int x, int y, bool state)
+        {
+            if(ValidateCellPosition(x, y))
+                field[x, y] = state;
+        }
+
+        private int CountNeighbors(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int col = (x + i + cols) % cols; // like earth globe (from the left side comes right side)
+                    int row = (y + j + rows) % rows;
+
+                    bool isSelfChecking = (col == x && row == y);
+                    bool hasLife = field[col, row];
+
+                    if (hasLife == true && isSelfChecking == false)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+        private bool ValidateCellPosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+    }
+}
